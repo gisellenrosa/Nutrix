3795364afa46a56b9f60ff7b2851c5d8f217ea32
gisellenrosa <gisellenrosa@gmail.com>
2022-06-23 22:23:54 -0300

    bugfix

--- src/pages/nutritionalResults/script.js
+++ src/pages/nutritionalResults/script.js
@@ -1,7 +1,7 @@
 let nextButton = document.getElementById('next-button');
 
 nextButton.onclick = function() {
-    window.location = '../mealsPerDay';
+    window.location = '../mealsperDay';
 }
 
 var imc = localStorage.getItem("imc")

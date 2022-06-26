7ffeb1b56aeb3fc59952962f9d57683454a78da2
gisellenrosa <gisellenrosa@gmail.com>
2022-06-26 12:57:09 -0300

    presentation

--- src/pages/nutritionalResults/script.js
+++ src/pages/nutritionalResults/script.js
@@ -1,7 +1,7 @@
 let nextButton = document.getElementById('next-button');
 
 nextButton.onclick = function() {
-    window.location = '../mealsperDay';
+    window.location = '../mealserDpay';
 }
 
 var imc = localStorage.getItem("imc")
